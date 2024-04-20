ad1be7a631c1581efaf7d9c03ce88100f7bb636a
rdxUAQ <157053122+rdxUAQ@users.noreply.github.com>
2024-04-19 20:48:09 -0600

    added service interface controller appusers

---
+++ Services/IApplicationUsersService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Hackathon_2024_API.Models;
+using Hackathon_2024_API.Schemas;
+
+
+namespace Hackathon_2024_API.Services
+{
+    public interface IApplicationUsersService
+    {
+        Task<ApplicationUser> CreateUserAsync(ApplicationUsersSchema applicationUsersSchema);
+        Task<ApplicationUser?> GetUserByEmailAsync(string email);
+        
+
+    }
+}
